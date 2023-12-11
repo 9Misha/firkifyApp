f1312cf68895989587103c935f76fa97caec2dda
mykhailo_yeroshevych <yeroshevich.misha@gmail.com>
2023-12-11 16:26:18 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '58627902-efa2-44fa-a5d7-35496d90ccbc';
+export const MODEL_CLOSE_SEC = 2.5;
